e434700f84d542931b7a1753443ccc6241d0b486
isaac1995514 <isaackleong514@gmail.com>
2023-08-23 21:45:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# image-carousel
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-ryhubz)
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+   
+  );
+}
