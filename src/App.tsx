e70d3a6d8bc645d4867750515dbedc9be9ec549e
isaac1995514 <isaackleong514@gmail.com>
2023-08-23 21:51:01 -0400

    1 file modified, 5 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
+import { ImageCarousel } from './components/image-carousel'
 import './style.css';
 
 export default function App() {
   return (
-   
+    <div
+      className="app-container"
+      style={{ height: '40%', width: '100%' }}
+    >
+      <ImageCarousel/>
+    </div>
   );
 }
---
+++ src/components/image-carousel/image-carousel.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+interface ImageCarouselProps {
+
+}
+
+export const ImageCarousel: React.FC<ImageCarouselProps> = ({}) => {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
